4bccb4ef405c2b6e946b221c03afb7774fe1e017
anusreepr99 <anusrepr99@gmail.com>
2021-05-26 17:24:00 +0530

    java

---
+++ first 15 programs/Avg.java
@@ -0,0 +1,10 @@
+public class Avg{
+public static void main(String args[])
+{
+double a=3.4,b=4.6,c=5.2,d=6.8,e=5.0,sum=0,avg=0;
+sum=a+b+c+d+e;
+avg=sum/5;
+System.out.println("sum of the 5 nos:"+sum);
+System.out.println("average of 5 nos:"+avg);
+}
+}
