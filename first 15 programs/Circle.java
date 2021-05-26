22c5902d8a0d552981d0cb5618470049c42c657c
anusreepr99 <anusrepr99@gmail.com>
2021-05-26 12:38:39 +0530

    java2

---
+++ first 15 programs/Circle.java
@@ -0,0 +1,10 @@
+class Circle{
+public static void main(String args[])
+{
+double r=7.5,a,p;
+a=3.14*7.5*7.5;
+p=2*3.14*7.5;
+System.out.println("perimeter: "+p);
+System.out.println("area: "+a);
+}
+}
