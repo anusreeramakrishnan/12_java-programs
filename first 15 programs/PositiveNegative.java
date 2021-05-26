d4fe734f807a9580a906030b51b335bd1ea5e2f9
anusreepr99 <anusrepr99@gmail.com>
2021-05-26 13:14:34 +0530

    java

---
+++ first 15 programs/PositiveNegative.java
@@ -0,0 +1,19 @@
+public class PositiveNegative  
+{  
+public static void main(String[] args)   
+{  
+int num=12;  
+if(num>0)  
+{  
+System.out.println(num + "The number is positive.");  
+}  
+else if(num<0)  
+{  
+System.out.println(num + "The number is negative.");  
+}  
+else  
+{  
+System.out.println("The number is zero.");  
+}  
+}  
+}
