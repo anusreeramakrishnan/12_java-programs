5a3b21980fdba91df41f6d8c69486c985ad37ac5
anusreepr99 <anusrepr99@gmail.com>
2021-09-24 10:29:26 +0530

    event

---
+++ CO5/pgm8/Key.java
@@ -0,0 +1,32 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+
+/*
+<applet code="Key" width=400 height=400>
+</applet>
+*/
+
+public class Key extends Applet implements KeyListener{
+String msg="key";
+int x=10,y=20;
+public void init(){
+addKeyListener(this);
+setBackground(Color.yellow);
+setForeground(Color.red);
+}
+
+public void keyPressed(KeyEvent ke){
+showStatus("key Down");
+}
+public void keyReleased(KeyEvent ke){
+showStatus("key up");
+}
+public void keyTyped(KeyEvent ke){
+msg+=ke.getKeyChar();
+repaint();
+}
+public void paint(Graphics g){
+g.drawString(msg,x,y);
+}
+}
